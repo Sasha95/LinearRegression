56b92105fc786f28aad48b6a3ee0fc0764acb2a2
Sasha95 <sashamangir@mail.ru>
2018-07-09 02:45:07 +0300

    first commit

---
+++ LinearRegression/LinearRegression.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Forms;
+
+
+namespace LinearRegression
+{
+    public partial class LinearRegression : Form
+    {
+        public LinearRegression()
+        {
+            InitializeComponent();            
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Clear(); //очищаем график, чтоб при новом клике на кнопку стирался предыдущий график
+            chart1.Series[1].Points.Clear();
+            int n = 30;  //кол-во точек в массиве. Тут сделай под себя, это значение может меняться динамически. Главное, чтоб кол-во X и Y было одинаковым
+
+            double[] xVals = new double[n];
+            double[] yVals = new double[n];
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < n; i++) 
+            {
+                xVals[i] = i; //задаем массив X
+                yVals[i] = i + rnd.Next(-5, 5);//задаем некоторое отклонение от прямой, это наш массив Y
+            }
+
+            SimpleLinearRegression slr = new SimpleLinearRegression(xVals, yVals); //Создаем экземпляр класса и передаем в конструктор созданные выше массивы X и Y 
+
+            for (int i = 0; i < yVals.Length; i++)
+            {
+                chart1.Series[0].Points.AddXY(xVals[i], yVals[i]); //рисуем точки с некоторым отклонением от прямой (их задали выше)
+                chart1.Series[1].Points.AddXY(xVals[i], slr.PredictY(i)); //рисуем те же X и предсказанные Y
+            }           
+        }
+    }
+}
---
+++ LinearRegression/SimpleLinearRegression.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+
+public class SimpleLinearRegression
+{
+    public double[] xVals, yVals;
+    public double standardDevX, standardDevY, correlation, slope, interception; //стандартное отклонение X и Y, пирсонская корреляция, угол наклона графика, отступ от оси абсцисс по оси ординат 
+
+    public SimpleLinearRegression(double[] xVals, double[] yVals)  //конструктор класса
+    {
+        this.xVals = xVals;
+        this.yVals = yVals;
+        standardDevX = GetStandardDeviation(xVals);
+        standardDevY = GetStandardDeviation(yVals);
+        correlation = GetCorrelation(xVals, yVals);
+        //Это из теории
+        slope = correlation * standardDevY / standardDevX;
+        interception = GetArrMean(yVals) - slope * GetArrMean(xVals);
+    }
+
+    public double PredictY(double x) //предсказываем Y по X
+    {
+        return interception + slope * x;
+    }
+
+    private static double GetArrMean(double[] arr) //ср. значение
+    {
+        return arr.Sum() / arr.Length;
+    }
+
+    private static double GetStandardDeviation(double[] arr) //стандартное отклонение
+    {
+        double mean = GetArrMean(arr);
+        double[] devs = new double[arr.Length]; 
+        for (int i = 0; i < arr.Length; i++)
+        {
+            devs[i] = Math.Pow(arr[i] - mean, 2);
+        }
+        return Math.Sqrt(devs.Sum() / (devs.Length - 1));
+    }
+
+    private static double GetCorrelation(double[] X, double[] Y) //расчет корреляции
+    {
+        double XMean = X.Sum() / X.Length;
+        double YMean = Y.Sum() / Y.Length;
+        double[] x = new double[X.Length];
+        double[] y = new double[Y.Length];
+
+        for (int i = 0; i < X.Length; i++)
+        {
+            x[i] = X[i] - XMean;
+            y[i] = Y[i] - YMean;
+        }
+
+        double[] xy = new double[X.Length];
+        for (int i = 0; i < X.Length; i++)
+        {
+            xy[i] = x[i] * y[i];
+        }
+
+        double[] xPowed = new double[X.Length];
+        double[] yPowed = new double[Y.Length];
+
+        for (int i = 0; i < X.Length; i++)
+        {
+            xPowed[i] = Math.Pow(x[i], 2);
+            yPowed[i] = Math.Pow(y[i], 2);
+        }
+
+        return xy.Sum() / Math.Sqrt(xPowed.Sum() * yPowed.Sum());
+    }
+}
+
